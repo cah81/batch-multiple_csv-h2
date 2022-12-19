3a900378844bcac3b4934448c081837ec42e96c4
carlos Angulo <carlosenrique.angulo@bosonit.com>
2022-12-19 10:42:13 +0100

    configuracion salida de archivo csv en segundo paso

--- src/main/java/com/springbatch.multiplecsv/batch/infraestructure/jobs/config/BatchConfig.java
+++ src/main/java/com/springbatch.multiplecsv/batch/infraestructure/jobs/config/BatchConfig.java
@@ -8,14 +8,18 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 @Configuration
@@ -30,12 +34,17 @@ public class BatchConfig {
     @Value("classpath*:/data/inputData*.csv")
     private Resource[] inputResources;
 
+    //para sacar la informacion en archivo
+
+    private Resource outPutResource = new FileSystemResource("output/outputData.csv");
+
     @Bean
     public Job readCSVFilesJob(){
         return jobBuilderFactory
                 .get("readCSVFilesJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
+                .next(step2())
                 .build();
 
     }
@@ -87,4 +96,36 @@ public class BatchConfig {
       public ConsoleItemWriter writer(){
             return new ConsoleItemWriter();
       }
+      @Bean
+      public FlatFileItemWriter<Employee> writerFlatPlane(){
+
+        //CREATE WRITER INSTANCE
+          FlatFileItemWriter<Employee> writer = new FlatFileItemWriter<>();
+          //setup output file location
+
+          writer.setResource(outPutResource);
+          //All job repetitions should "append" to same output file
+          writer.setAppendAllowed(true);
+          //Name field values sequence based on object properties
+          writer.setLineAggregator(new DelimitedLineAggregator<Employee>(){
+              {
+                  setDelimiter(",");
+                  setFieldExtractor(new BeanWrapperFieldExtractor<Employee>(){
+                      {
+                          setNames(new String [] {"id","firstName","lastName"});
+                      }
+                  });
+              }
+          });
+          return writer;
+
+      }
+      @Bean
+      public Step step2(){
+        return stepBuilderFactory.get("step2")
+                .<Employee,Employee>chunk(5)
+                .reader(multiResourceItemReader())
+                .writer(writerFlatPlane())
+                .build();
+    }
 }
